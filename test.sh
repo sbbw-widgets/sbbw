6b1a6b95bb92512e17a7af2b0176b1c93e629198
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2022-01-30 00:00:13 -0400

    feat: add test automatization script

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "name = "Test"\
+class_name = "Test_Class"\
+width = "200.0"\
+height = "Max"\
+x = 0.0\
+y = 0.0\
+transparent = true\
+blur = true\
+allways_on_top = true\
+" > /tmp/test_config.toml
