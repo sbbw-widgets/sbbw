ca02a89dda4052be8d26ffeab65c458fc88078f0
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2022-01-30 22:08:20 -0400

    fix: fix all script and migrate to this new modularize structure

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Verify if yarn is installed
-if ! command -v yarn >/dev/null 2>&1; then
-  echo "Yarn is not installed"
-  exit 1
-fi
-
 # Verify if cargo is installed
 if ! command -v cargo >/dev/null 2>&1; then
   echo "Cargo or Rust is not installed"
@@ -14,6 +8,3 @@ fi
 
 # install dependencies with cargo in sbbw-daemon
 cargo build --release
-
-# install dependencies with yarn in sbbw-window
-cd sbbw-window && yarn tauri build
--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Verify if cargo is installed
+if ! command -v cargo >/dev/null 2>&1; then
+  echo "Cargo or Rust is not installed"
+  exit 1
+fi
+
 cat << EOF >> /tmp/test_config.toml
 name = "Test"
 class_name = "Test_Class"
@@ -11,3 +17,5 @@ transparent = true
 blur = true
 always_on_top = true
 EOF
+
+cargo test
