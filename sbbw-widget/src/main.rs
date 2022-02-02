8ec716edf998223cc21701a8ae72289ee9d2d8dc
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2022-02-02 15:47:25 -0400

    feat: set values from settings file and create window with correct load widget ui

--- sbbw-widget/src/main.rs
+++ sbbw-widget/src/main.rs
@@ -4,11 +4,14 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+mod exts;
+use exts::*;
+
 use std::env;
 
 use colored::*;
-use sbbw_widget_conf::{get_widgets, get_widgets_path};
-use tauri::{generate_context, App, Manager, WindowUrl};
+use sbbw_widget_conf::{get_widgets, get_widgets_path, WidgetSize};
+use tauri::{generate_context, App, LogicalSize, Manager, Size, WindowUrl, PhysicalSize};
 use tauri_plugin_positioner::Positioner;
 use tauri_plugin_vibrancy::Vibrancy;
 
@@ -23,27 +26,63 @@ fn main() {
             let path_to_widget_conf = get_widgets_path().join(&widget_name).join("config.toml");
             let widget_conf = sbbw_widget_conf::validate_config_toml(path_to_widget_conf).unwrap();
 
-            tauri::Builder::default()
-                .setup(move |app| {
-                    let url_ui = format!("http://localhost:8000/{}/ui", widget_name);
-                    app.create_window(
-                        "main",
-                        WindowUrl::External(url::Url::parse(url_ui.as_str()).unwrap()),
-                        |w_builder, attr| {
-                            let widget_conf = widget_conf.clone();
-                            let builder = w_builder.clone()
-                                .resizable(false)
-                                .decorations(false)
-                                .skip_taskbar(true)
-                                .title(&widget_conf.name)
-                                .position(widget_conf.x as f64, widget_conf.y as f64)
-                                .always_on_top(widget_conf.always_on_top)
-                                .transparent(widget_conf.transparent);
-                            (builder, attr)
-                        },
-                    ).unwrap();
-                    if widget_conf.blur {
-                        let window = app.get_window("main").unwrap();
+            let main_app = tauri::Builder::default()
+                .plugin(Positioner::default())
+                .build(generate_context!())
+                .unwrap();
+
+                let url_ui = format!("http://localhost:8000/{}/ui", widget_name);
+                let app_handle = main_app.handle();
+                let widget_conf_clone = widget_conf.clone();
+                tauri::async_runtime::spawn(async move {
+                    app_handle
+                        .clone()
+                        .create_window(
+                            "main",
+                            WindowUrl::External(url::Url::parse(url_ui.as_str()).unwrap()),
+                            |w_builder, attr| {
+                                let builder = w_builder
+                                    .clone()
+                                    .title(&widget_conf.name)
+                                    .decorations(false)
+                                    .skip_taskbar(true)
+                                    .title(&widget_conf_clone.name)
+                                    .position(
+                                        widget_conf_clone.x as f64,
+                                        widget_conf_clone.y as f64,
+                                    )
+                                    .fullscreen(widget_conf_clone.width == WidgetSize::Max && widget_conf_clone.height == WidgetSize::Max)
+                                    .always_on_top(widget_conf_clone.always_on_top)
+                                    .transparent(widget_conf_clone.transparent);
+                                (builder, attr)
+                            },
+                        )
+                        .unwrap();
+                    let window = app_handle.get_window("main").unwrap();
+                    let monitor_size = *window.current_monitor().unwrap().unwrap().size();
+                    let width = match widget_conf_clone.width {
+                        WidgetSize::Max => {
+                            monitor_size.width as f64
+                        }
+                        WidgetSize::Value(v) => v,
+                    };
+                    let height = match widget_conf_clone.height {
+                        WidgetSize::Max => {
+                            monitor_size.height as f64
+                        }
+                        WidgetSize::Value(v) => v,
+                    };
+                    println!("Height: {}", format!("{}", &height).green());
+                    let size = Size::Logical(LogicalSize { width, height });
+                    window.set_min_size(Some(size)).unwrap();
+                    window.set_max_size(Some(size)).unwrap();
+                    window.set_size(size).unwrap();
+                    window.set_role(&widget_conf.name, &widget_conf.class_name);
+                    window.set_resizable(false).unwrap();
+                    if widget_conf_clone.stick {
+                        window.stick();
+                    }
+                    if widget_conf_clone.blur {
                         #[cfg(target_os = "windows")]
                         window.apply_acrylic();
                         #[cfg(target_os = "macos")]
@@ -54,10 +93,9 @@ fn main() {
                             );
                         }
                     }
-                    Ok(())
-                })
-                .run(generate_context!())
-                .expect("error while running tauri application");
+                });
+
+                main_app.run(|_app_handler, _event| { });
         } else {
             println!(
                 "{}",
