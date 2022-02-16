68cf72d91a5ed4f19d9a478412f6635a8b69155c
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2022-02-16 17:34:00 -0400

    fix(setup_bat): fix batch file for automate setup on windows system

--- setup/setup.bat
+++ setup/setup.bat
@@ -7,43 +7,47 @@ goto check_Permissions
 
     net session >nul 2>&1
     if not %errorLevel% == 0 (
-        echo Failure: Run as administrator.
+        echo [ X ] Run as administrator.
         pause >nul
         exit /b 1
     )
 
 
 SET PATH_TO_INSTALL=C:\Program Files\sbbw
+SET CURRENT_DIR=%~dp0
 
-echo "Installing sbbw"
-sleep 2
+echo Installing sbbw
+timeout /t 2 > NUL
 
-IF exist "%PATH_TO_INSTALL%\sbbw.exe" ]] (
-  echo "sbbw already installed"
-  exit 1
+if exist "%PATH_TO_INSTALL%\sbbw.exe" (
+    echo sbbw already installed
+    pause >nul
+    exit 1
 )
-IF not exist sbbw.exe (
-  echo "sbbw binary not found"
-  exit 1
+if not exist "%CURRENT_DIR%\sbbw.exe" (
+    echo sbbw binary not found
+    pause >nul
+    exit 1
 )
-IF not exist sbbw-widget.exe (
-  echo "sbbw-widget binary not found"
-  exit 1
+if not exist "%CURRENT_DIR%\sbbw-widget.exe" (
+    echo sbbw-widget binary not found
+    pause >nul
+    exit 1
 )
 
 :: Add sbbw to PATH
-IF not exist "%PATH_TO_INSTALL%" (
-  mkdir "%PATH_TO_INSTALL%"
+if not exist "%PATH_TO_INSTALL%" (
+    mkdir "%PATH_TO_INSTALL%"
 )
 
 setx /M PATH "%PATH%;%PATH_TO_INSTALL%"
 
-copy sbbw.exe "%PATH_TO_INSTALL%"
-copy sbbw-widget.exe "%PATH_TO_INSTALL%"
+copy "%CURRENT_DIR%\sbbw.exe" "%PATH_TO_INSTALL%"
+copy "%CURRENT_DIR%\sbbw-widget.exe" "%PATH_TO_INSTALL%"
 
-echo "[ ✓ ] Sbbw Installed"
-sleep 2
+echo [ OK ] Sbbw Installed
+timeout /t 2 > NUL
 
-echo "[ ✓ ] Done, you can close this"
+echo [ OK ] Done, you can close this
 
 pause >nul
