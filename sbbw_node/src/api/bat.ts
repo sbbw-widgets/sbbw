83ad306e9e0329d68dc15c1a9003541c4a328d56
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2022-08-25 12:33:28 -0400

    feat: add battery module api

---
+++ sbbw_node/src/api/bat.ts
@@ -0,0 +1,91 @@
+type SbbwBatteryState = 'unknown' |
+    'charging' | 'discharging' |
+    'empty' | 'full'
+
+type SbbwBatteryTechnology = 'Unknown' |
+'LithiumIon' | 'LeadAcid' |
+'LithiumPolymer' | 'NickelMetalHydride' |
+'NickelCadmium' | 'NickelZinc' |
+'LithiumIronPhosphate' | 'RechargeableAlkalineManganese'
+
+type SbbwBattery = {
+    /**
+     * Battery vendor
+     */
+    vendor: string,
+    /**
+     * Battery model
+     */
+    model: string,
+    /**
+     * Battery serial number
+     */
+    serial: string,
+    /**
+     * Percent of energy currently available in the battery
+     */
+    percentage: number,
+    /**
+     * Amount of energy currently available in the battery
+     */
+    energy: number,
+    /**
+     * Amount of energy in the battery when it's considered full
+     */
+    energy_full: number,
+    /**
+     * Battery voltage
+     */
+    voltage: number,
+    /**
+     * Battery current state.
+     */
+    state: SbbwBatteryState | string,
+    /**
+     * Gets battery state of health
+     * For more details. See also: [State of health](https://en.wikipedia.org/wiki/State_of_health) or [soh](https://www.mpoweruk.com/soh.htm)
+     */
+    health: number,
+    /**
+     * Battery technology
+     */
+    technology: SbbwBatteryTechnology | string,
+    /**
+     * Battery temperature
+     */
+    temperature: number,
+    /**
+     * Number of charge/discharge cycles
+     */
+    cycle_count: number,
+    /**
+     * Remaining time till full battery
+     */
+    time_to_full: number,
+    /**
+     * Remaining time till empty battery
+     */
+    time_to_empty: number,
+}
+
+/**
+ * @returns A promise with count of batteries
+ */
+const getBatteryCounts = (): Promise<number> =>
+    window.rpc.call("battery.counts", [])
+
+
+/**
+ * @returns A promise with array of all batteries data
+ */
+const getAllBatteries = (): Promise<Array<SbbwBattery>> =>
+    window.rpc.call("battery.all", [])
+
+/**
+ * @returns A promise with array of all batteries data
+ */
+const getMainBattery = (): Promise<SbbwBattery> =>
+    window.rpc.call("battery.main", [])
+
+export type { SbbwBattery, SbbwBatteryState, SbbwBatteryTechnology }
+export { getBatteryCounts, getAllBatteries, getMainBattery }
